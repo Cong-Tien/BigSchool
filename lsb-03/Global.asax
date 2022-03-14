d63e564be8e08511b9e13521f591fa92758e4367
Cong-Tien <letien18102001@gmail.com>
2022-03-14 16:53:34 +0700

    first commit

---
+++ lsb-03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lsb_03.MvcApplication" Language="C#" %>
---
+++ lsb-03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lsb_03.Startup))]
+namespace lsb_03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
