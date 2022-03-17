2652ed8f37d58c51abc0fade2c6cbd5c6bf8e93b
Cong-Tien <letien18102001@gmail.com>
2022-03-17 10:37:59 +0700

    Adding for lab3

---
+++ lsb-03/Controllers/CoursesController.cs
@@ -0,0 +1,25 @@
+﻿using lsb_03.Models;
+using lsb_03.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lsb_03.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext = new ApplicationDbContext();
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
